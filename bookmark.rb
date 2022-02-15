8e28ca0e9e3e1ca39b4e6ad2a5cbde11c285396b
Haroon <haroon20@hotmail.co.uk>
2022-02-15 12:17:52 +0000

    Step 3 mid way

--- bookmark.rb
+++ bookmark.rb
@@ -10,5 +10,19 @@ class BookmarkManager < Sinatra::Base
     'Bookmark Manager'
   end
 
+  get '/bookmarks' do
+    @bookmarks = [
+            "http://www.makersacademy.com",
+            "http://www.destroyallsoftware.com",
+            "http://www.google.com"
+           ]
+
+     erb :'bookmarks/index'
+  end
+
+
+
+
+
   run! if app_file == $0
 end
---
+++ spec/features/viewing_bookmark_spec.rb
@@ -0,0 +1,8 @@
+feature 'view bookmarks' do
+    scenario 'As a user I can see bookmarks' do
+        visit('/bookmarks')
+        expect(page).to have_content "http://www.makersacademy.com"
+        expect(page).to have_content "http://www.destroyallsoftware.com"
+        expect(page).to have_content "http://www.google.com"
+    end
+end

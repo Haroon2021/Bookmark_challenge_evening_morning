541c4c80f33b27f409ec2428e1f7c647c8f5ea94
Rakshak Shetty <rakshak_81@hotmail.com>
2022-02-18 21:49:10 +0000

    Step 10 Completed

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,18 +4,29 @@ require 'pg'
 describe Bookmark do
   describe '.all' do
     it "returns all bookmarks" do
-      connection = PG.connect(dbname: 'bookmark_manager_test')
+      # connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      #Add test data 
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      # #Add test data 
+      # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+      # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+      # connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
+      Bookmark.create(url: "http://www.makersacademy.com")
+      Bookmark.create(url: "http://www.destroyallsoftware.com")
+      Bookmark.create(url: "http://www.google.com")
 
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("http://www.makersacademy.com")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")  
+    end
+  end
+
+  describe '.create' do
+    it 'Adds new bookmarks' do
+      Bookmark.create(url: 'http://www.example.org' )
+      expect(Bookmark.all).to include 'http://www.example.org'
     end
   end
 end
--- spec/features/Adding_bookmarks_spec.rb
+++ spec/features/Adding_bookmarks_spec.rb
@@ -1,8 +1,8 @@
 feature 'To add a new bookmark' do
     scenario 'As a user I want to add bookmarks' do
         visit('/bookmarks/new')
-        fill_in('url', with: 'www.google.com')
+        fill_in('url', with: 'http://www.google.com')
         click_button('Submit')
-        expect(page).to have_content 'www.google.com'
+        expect(page).to have_content 'http://www.google.com'
     end
 end
